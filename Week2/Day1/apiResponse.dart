880d929fbaeb590f94495211b93d5c19e10a0757
sadmanhossainroudro <sadmanhossainroudro@gmail.com>
2026-01-21 00:34:15 +0600

    eat: implemented generic api response class

--- Week2/Day1/apiResponse.dart
+++ Week2/Day1/apiResponse.dart
@@ -6,9 +6,17 @@ class ApiResponse<T> {
   ApiResponse(this.data, this.error, this.loading);
 }
 
-void main() {
+class User {
+  String name;
+  User(this.name);
+}
 
-  
+void main() {
+  var user1 = User("Roudro");
+
+  var response = ApiResponse(user1, null, true);
+  print(response.data);
+  print(response.data?.name);
 
   var str = ApiResponse("This is a String", "String Error", true);
   print(str.data);
